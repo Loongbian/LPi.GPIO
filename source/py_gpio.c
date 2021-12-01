48e00c8e67d05f3913ffdbd3d083df3f10ce8f8a
longpengan <longpengan@loongson.cn>
2021-12-01 18:09:10 +0800

    03add cleanup setmode

--- source/py_gpio.c
+++ source/py_gpio.c
@@ -1,5 +1,26 @@
 #include <Python.h>
 
+// python function cleanup(channel=None)
+static PyObject *py_cleanup(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+	Py_RETURN_NONE;
+}
+
+// python function setup(channel(s), direction, pull_up_down=PUD_OFF, initial=None)
+static PyObject *py_setup_channel(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+
+	Py_RETURN_NONE;
+
+}
+
+// python function setmode(mode)
+static PyObject *py_setmode(PyObject *self, PyObject *args)
+{
+
+   Py_RETURN_NONE;
+}
+
 // Function 1: A simple 'hello world' function
 static PyObject* helloworld(PyObject* self, PyObject* args)
 {
@@ -12,6 +33,11 @@ static PyObject* helloworld(PyObject* self, PyObject* args)
 // definition
 static PyMethodDef myMethods[] = {
     { "helloworld", helloworld, METH_NOARGS, "Prints Hello World" },
+    {"setup", (PyCFunction)py_setup_channel, METH_VARARGS | METH_KEYWORDS, "Set up a GPIO channel or list of channels with a direction and (optional) pull/up down control\nchannel        - either board pin number or BCM number depending on which mode is set.\ndirection      - IN or OUT\n[pull_up_down] - PUD_OFF (default), PUD_UP or PUD_DOWN\n[initial]      - Initial value for an output channel"},
+    {"cleanup", (PyCFunction)py_cleanup, METH_VARARGS | METH_KEYWORDS, "Clean up by resetting all GPIO channels that have been used by this program to INPUT with no pullup/pulldown and no event detection\n[channel] - individual channel or list/tuple of channels to clean up.  Default - clean every channel that has been used."},
+    {"setmode", py_setmode, METH_VARARGS, "Set up numbering mode to use for channels.\nBOARD - Use Raspberry Pi board numbers\nBCM   - Use Broadcom GPIO 00..nn numbers"},
+
+
     { NULL, NULL, 0, NULL }
 };
 
--- test/test.py
+++ test/test.py
@@ -2,6 +2,25 @@ import unittest
 import LPi as GPIO
 
 
+LED_PIN = 12
+
+# Test starts with 'AAA' so that it is run first
+class TestAAASetup(unittest.TestCase):
+    def runTest(self):
+        # Test mode not set (BOARD or BCM) exception
+        #with self.assertRaises(RuntimeError) as e:
+        #    GPIO.setup(LED_PIN, GPIO.OUT)
+        #self.assertEqual(str(e.exception), 'Please set pin numbering mode using GPIO.setmode(GPIO.BOARD) or GPIO.setmode(GPIO.BCM)')
+
+        # Test trying to change mode after it has been set
+        GPIO.setmode(GPIO.BCM)
+        with self.assertRaises(ValueError) as e:
+            GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(LED_PIN_BCM, GPIO.IN)
+        GPIO.cleanup()
+
+
+
 class Test(unittest.TestCase):
     def runTest(self):
         GPIO.helloworld()
